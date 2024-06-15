14130b358919fc2c96d5f55c4376621e33e350ce
Scot Kennedy <gateless@gmail.com>
2024-06-15 13:37:16 -0700

    Updates and edits

--- notebooks/lightningcatcher_paraview.py
+++ notebooks/lightningcatcher_paraview.py
@@ -17,9 +17,11 @@ def animate(config, file_paths, framerate="2", remove_anim_frames=False):
     project_name = config['project_name']
     for i,file_path in enumerate(file_paths):
         shutil.copyfile(os.path.join(base_dir,file_path), os.path.join(base_dir, output_dir, f"animation_frame-{i:0>3}.png"))
-    subprocess.run([ffmpeg_path, "-r", framerate,  "-y", "-i", 
-                    f"{config['base_dir']}/{project_name}/animation_frame-%03d.png", 
-                    f"{config['base_dir']}/{project_name}/{project_name}.gif"])
+    with open(f"{config['project_name']}/{config['case_name']}-animation.log", "w") as outfile:
+        subprocess.run([ffmpeg_path, "-r", framerate,  "-y", "-i", 
+                        f"{config['base_dir']}/{project_name}/animation_frame-%03d.png", 
+                        f"{config['base_dir']}/{project_name}/{project_name}.gif"],
+                      stdout=outfile,stderr=outfile)
     if remove_anim_frames:
         for i,file_path in enumerate(file_paths):
            os.remove(os.path.join(output_dir, f"animation_frame_{i:0>3}.png"))
@@ -38,7 +40,7 @@ def visualize_case(project_dir, config, base_vis_path="para.py", text="\"Flow Vi
     with open(f"{config['project_name']}/{config['case_name']}-paraview.log", "w") as outfile:
         subprocess.run([pvpath, base_vis_path, 
                         f"{project_dir}/{config['case_name']}", text,
-                       config['base_dir']],stdout=outfile)
+                       config['base_dir']],stdout=outfile,stderr=outfile)
     return f"{project_dir}/{config['case_name']}-para.png"
     #return f"{config['base_dir']}/{project_dir}/{config['case_name']}-para.png"
     
